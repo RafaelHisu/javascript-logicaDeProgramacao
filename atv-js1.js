84c4ae03bd0450d6e906d6e9bac7d415684ed52e
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-29 19:40:29 -0300

    Create

---
+++ atv-js1.js
@@ -0,0 +1,13 @@
+var n1 = Number (prompt("Insira um número por favor:"));
+
+if (n1 < 0) {
+    document.write (`O número ${n1}  é negativo.`);
+} else if (n1 > 0) {
+        document.write (`O número ${n1}  é positivo.`);
+    } else if (n1 == 0) {
+        document.write (`O número é ${n1}.`);
+    }
+    
+
+
+
