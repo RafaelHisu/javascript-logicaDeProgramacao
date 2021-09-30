099ef887f68a96cac14e578f6173a0cb29a7efd8
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-30 15:16:49 -0300

    Atv-9

--- atv-js4.js
+++ atv-js4.js
@@ -3,6 +3,6 @@ var n1 = +prompt("Insira um 2º valor:");
 var n2 = +prompt("Insira um 3º valor:");
 var soma = 0;
 
-if (n > n1 > n2) {
+if (n > n1  && n > n2) {
 
 }
---
+++ atv-js9.js
@@ -0,0 +1,7 @@
+var n = 100;
+
+while (n <= 110) {
+       document.write (`<br> ${n}`);
+       n++;
+    
+}
