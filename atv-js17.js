a9f0652584117f9741584930115bca780e92e677
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-30 19:09:28 -0300

    Atv-17 att

--- atv-js17.js
+++ atv-js17.js
@@ -1,1 +1,12 @@
-var idadeGato
+var idadeGato = 7;
+var idade = 0;
+var n = 3;
+var primeiroAno = 15;
+var segundoAno = primeiroAno + 10;
+var proximosAnos = segundoAno;
+
+while (n <= 7) {
+    proximosAnos = proximosAnos + 4;
+    n++
+}
+document.write (`A idade de uma gato em 7 anos humanos é ${proximosAnos}.`)
