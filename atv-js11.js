9f1e44295fdf5a84d3abc4b81d89ba433935789a
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-30 15:47:44 -0300

    Atv-11

---
+++ atv-js11.js
@@ -0,0 +1,14 @@
+var n = +prompt(`Insira um valor:`);
+var n1 = +prompt(`Insira um valor:`);
+var n2 = +prompt(`Insira um valor:`);
+var n3 = +prompt(`Insira um valor:`);
+var n4 = +prompt(`Insira um valor:`);
+var n5 = +prompt(`Insira um valor:`);
+var n6 = +prompt(`Insira um valor:`);
+var n7 = +prompt(`Insira um valor:`);
+var n8 = +prompt(`Insira um valor:`);
+var n9 = +prompt(`Insira um valor:`);
+
+var media = ((n + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 +n9) / 10)
+
+document.write (`A média aritmética dos números ${n} + ${n1} + ${n2} + ${n3} + ${n4} + ${n5} + ${n6} + ${n7} + ${n8} + ${n9} é igual a ${media}.`);
