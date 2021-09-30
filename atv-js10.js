13c521a9c53ec07d29088c449c6fce0ccf130778
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-30 15:39:43 -0300

    atv-10

---
+++ atv-js10.js
@@ -0,0 +1,13 @@
+var n = parseInt(prompt(`Insira um valor de 1 a 100:`));
+
+
+if (n >= 100 || n <= 0) {
+    alert(`Informe um número de 1 a 100.`);
+    valor();
+}
+
+while (n <= 100) {
+    document.write (` <br> ${n}`);
+    n++;
+}
+
