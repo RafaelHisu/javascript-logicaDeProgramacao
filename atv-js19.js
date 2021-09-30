6f137796e927c38a87cda283d81e0d7469839c5f
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-30 19:52:59 -0300

    atv-19 att

--- atv-js19.js
+++ atv-js19.js
@@ -2,9 +2,9 @@ var a = parseFloat(prompt(`Digite o valor(numérico) da variável A:`));
 var b = parseFloat(prompt(`Digite o valor(numérico) da variável B:`));
 var troca;
 
-troca = a;
-a = b;
-b = troca;
+troca = (a);
+a = (b);
+b = (troca);
 
-document.write (`O novo valor de A é ${troca}.`);
-document.write (`<br>O novo valor de B é ${troca}.`);
+document.write (`O novo valor de A é ${a}.`);
+document.write (`<br>O novo valor de B é ${b}.`);
