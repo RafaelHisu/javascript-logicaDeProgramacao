31b0b126a3232766301606b6d376fc25704304cc
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-09-30 16:32:54 -0300

    Atv-14

---
+++ atv-js14.js
@@ -0,0 +1,7 @@
+var n = +prompt(`Informe uma nota:`);
+var n1 = +prompt(`Informe uma nota:`);
+var n2 = +prompt(`Informe uma nota:`);
+var n3 = +prompt(`Informe uma nota:`);
+var media = ((n + n1 + n2 + n3) / 4);
+
+document.write (`Sua média é ${media}.`);
